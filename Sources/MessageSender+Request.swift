0e7c295d66efa6363b89623371c6ba5f02da53ab
Jun Tanaka <junpluse@gmail.com>
2016-12-05 15:07:33 +0900

    📝 Add documentation for MessageSender

--- Sources/MessageSender+Request.swift
+++ Sources/MessageSender+Request.swift
@@ -9,6 +9,15 @@
 import Foundation
 
 extension MessageSenderProtocol {
+	/// Send a request message to peers.
+	///
+	/// - Parameters:
+	///   - request: A request message to be sent.
+	///   - peers: An array of peers that should receive the request.
+	/// - Returns: A receipt of the submission.
+	/// - Throws: An `Error` if sending the request could not be completed.
+	/// - Note: Pass returned receipt to MessageReceiptProtocol.observeResponses()
+	///         to observe responses of the request.
 	@discardableResult
 	public func submit<T: RequestProtocol>(_ request: T, to peers: [Peer]) throws -> RequestReceipt<T, Self.Peer> {
 		try send(request, to: peers)
--- Sources/MessageSender.swift
+++ Sources/MessageSender.swift
@@ -8,29 +8,58 @@
 
 import Foundation
 
+/// Represents a message sender
 public protocol MessageSenderProtocol {
 	associatedtype Peer: PeerProtocol
 
+	/// Send an archived data of message to peers.
+	///
+	/// - Parameters:
+	///   - data: An archived data of message.
+	///   - message: The orignal message of the data.
+	///   - peers: An array of peers that should receive the data.
+	/// - Throws: An `Error` if sending the message could not be completed.
 	func send(_ data: Data, of message: MessageProtocol, to peers: [Peer]) throws
 }
 
+/// A struct that implements `MessageSenderProtocol` using a closure or
+/// another one that conforms `MessageSenderProtocol` to be wrapped.
 public struct MessageSender<Peer: PeerProtocol>: MessageSenderProtocol {
 	private let _action: (Data, MessageProtocol, [Peer]) throws -> Void
 
-	public init(_ send: @escaping (Data, MessageProtocol, [Peer]) throws -> Void) {
-		_action = send
+	/// Initializes a sender which sends data of messages using the given closure.
+	///
+	/// - Parameter action: A closure to send data of messages.
+	public init(_ action: @escaping (Data, MessageProtocol, [Peer]) throws -> Void) {
+		_action = action
 	}
 
-	public init<T: MessageSenderProtocol>(_ base: T) where T.Peer == Peer {
-		_action = { try base.send($0, of: $1, to: $2) }
+	/// Initializes a sender which wraps the given sender.
+	///
+	/// - Parameter sender: A sender to be wrapped.
+	public init<T: MessageSenderProtocol>(_ sender: T) where T.Peer == Peer {
+		_action = { try sender.send($0, of: $1, to: $2) }
 	}
 
-	public 	func send(_ data: Data, of message: MessageProtocol, to peers: [Peer]) throws {
+	/// Send an archived data of message to peers.
+	///
+	/// - Parameters:
+	///   - data: An archived data of message.
+	///   - message: The orignal message of the data.
+	///   - peers: An array of peers that should receive the data.
+	/// - Throws: An `Error` if sending the data could not be completed.
+	public func send(_ data: Data, of message: MessageProtocol, to peers: [Peer]) throws {
 		try _action(data, message, peers)
 	}
 }
 
 extension MessageSenderProtocol {
+	/// Send a message to peers.
+	///
+	/// - Parameters:
+	///   - message: A message to be sent.
+	///   - peers: An array of peers that should receive the message.
+	/// - Throws: An `Error` if sending the message could not be completed.
 	public func send<T: MessageProtocol>(_ message: T, to peers: [Peer]) throws {
 		let data = Archiver().archive(message)
 		try send(data, of: message, to: peers)
