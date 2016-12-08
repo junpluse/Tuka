9f06ca00a989db1655588ad392bb204232a6b4fb
Jun Tanaka <junpluse@gmail.com>
2016-12-09 00:17:19 +0900

    📝 Add documentation for MessageReceiverProtocol

--- Sources/MessageReceiver+Request.swift
+++ Sources/MessageReceiver+Request.swift
@@ -8,6 +8,12 @@
 
 import Foundation
 
+/// A `ResponseObservationEvent` value represents a event of response observation
+/// that produced by the message receiver.
+///
+/// - received: Indicates that the response from a peer has been received.
+/// - completed: Indicates that all responses from the peers has beed received.
+/// - timedOut: Indicates that the receiver timed out the observation of responses.
 public enum ResponseObservationEvent<Request: RequestProtocol, Peer: PeerProtocol> {
 	case received(Request.Response, from: Peer)
 	case completed([Peer: Request.Response])
@@ -15,6 +21,15 @@ public enum ResponseObservationEvent<Request: RequestProtocol, Peer: PeerProtoco
 }
 
 extension MessageReceiverProtocol {
+	/// Adds a handler that observe responses to the request.
+	///
+	/// - Parameters:
+	///   - request: A request for responses which should be observed.
+	///   - peers: An array of peers who should respond the request.
+	///   - interval: An number of seconds to wait for the observation to complete.
+	///   - queue: A dispatch queue to which closure should be added.
+	///   - handler: A closure to be executed when an observation event produced by the receiver.
+	/// - Returns: A `Disposable` which can be used to stop the invocation of the closure.
 	public func addObserver<T: RequestProtocol>(forResponsesTo request: T, from peers: [Peer], timeoutAfter interval: TimeInterval, on queue: DispatchQueue, handler: @escaping (ResponseObservationEvent<T, Peer>) -> Void) -> Disposable {
 		let disposable = CompositeDisposable()
 		if interval != .infinity {
@@ -41,12 +56,25 @@ extension MessageReceiverProtocol {
 		return disposable
 	}
 
+	/// Adds a handler that observe responses to the request on the given receipt.
+	///
+	/// - Parameters:
+	///   - receipt: A receipt of the request for the responses which should be observed.
+	///   - interval: An number of seconds to wait for the observation to complete.
+	///   - queue: A dispatch queue to which closure should be added.
+	///   - handler: A closure to be executed when the observation is completed or timed out.
+	/// - Returns: A `Disposable` which can be used to stop the invocation of the closure.
 	public func addObserver<T: RequestReceiptProtocol>(forResponsesToRequestOn receipt: T, timeoutAfter interval: TimeInterval, on queue: DispatchQueue, handler: @escaping (ResponseObservationEvent<T.Request, Peer>) -> Void) -> Disposable where T.Peer == Peer {
 		return addObserver(forResponsesTo: receipt.request, from: receipt.peers, timeoutAfter: interval, on: queue, handler: handler)
 	}
 }
 
 extension MessageReceiverProtocol where Self: MessageSenderProtocol {
+	/// Adds a responder for the request.
+	///
+	/// - Parameters:
+	///   - responder: A responder for the request.
+	/// - Returns: A `Disposable` which can be used to remove the responder from the receiver.
 	public func add<T: RequestResponderProtocol>(_ responder: T) -> Disposable where T.Peer == Peer {
 		return addObserver(for: T.Request.self, on: responder.responseQueue) { request, peer in
 			let response = responder.respond(to: request, from: peer)
@@ -58,6 +86,13 @@ extension MessageReceiverProtocol where Self: MessageSenderProtocol {
 		}
 	}
 
+	/// Adds a handler that respond to requests of the given type.
+	///
+	/// - Parameters:
+	///   - requestType: A type of request which should be responded to.
+	///   - queue: A dispatch queue to which closure should be added.
+	///   - handler: A closure to be executed when the request is received.
+	/// - Returns: A `Disposable` which can be used to stop the invocation of the closure.
 	public func addResponder<T: RequestProtocol>(for requestType: T.Type, on queue: DispatchQueue, handler: @escaping (T, Peer) -> T.Response) -> Disposable {
 		let responder = RequestResponder(queue: queue) { request, peer in
 			return handler(request, peer)
--- Sources/MessageReceiver.swift
+++ Sources/MessageReceiver.swift
@@ -8,29 +8,57 @@
 
 import Foundation
 
+/// Represents a message receiver
 public protocol MessageReceiverProtocol {
 	associatedtype Peer: PeerProtocol
 
+	/// Adds a received data handler with a dispatch queue and a closure to add to the queue.
+	///
+	/// - Parameters:
+	///   - queue: A dispatch queue to which closure should be added.
+	///   - handler: A closure to be executed when the data is received.
+	/// - Returns: A `Disposable` which can be used to stop the invocation of the closure.
 	func addDataObserver(on queue: DispatchQueue, handler: @escaping (Data, Peer) -> Void) -> Disposable
 }
 
+/// A struct that implements `MessageReceiverProtocol` using a closure or
+/// another one that conforms `MessageReceiverProtocol` to be wrapped.
 public struct MessageReceiver<Peer: PeerProtocol>: MessageReceiverProtocol {
 	private let _action: (DispatchQueue, @escaping (Data, Peer) -> Void) -> Disposable
 
+	/// Initializes a receiver which invoke the geven closure to add received data observers.
+	///
+	/// - Parameter action: A closure to add received data observers.
 	public init(_ action: @escaping (DispatchQueue, @escaping (Data, Peer) -> Void) -> Disposable) {
 		_action = action
 	}
 
+	/// Initializes a receiver which wraps the given receiver.
+	///
+	/// - Parameter receiver: A receiver to be wrapped.
 	public init<T: MessageReceiverProtocol>(_ base: T) where T.Peer == Peer {
 		_action = { base.addDataObserver(on: $0, handler: $1) }
 	}
 
+	/// Adds a received data handler with a dispatch queue and a closure to add to the queue.
+	///
+	/// - Parameters:
+	///   - queue: A dispatch queue to which closure should be added.
+	///   - handler: A closure to be executed when the data is received.
+	/// - Returns: A `Disposable` which can be used to stop the invocation of the closure.
 	public func addDataObserver(on queue: DispatchQueue, handler: @escaping (Data, Peer) -> Void) -> Disposable {
 		return _action(queue, handler)
 	}
 }
 
 extension MessageReceiverProtocol {
+	/// Adds a received message handler with a dispatch queue and a closure to add to the queue.
+	///
+	/// - Parameters:
+	///   - messageType: A type of message which should be observed.
+	///   - queue: A dispatch queue to which closure should be added.
+	///   - handler: A closure to be executed when a message is received.
+	/// - Returns: A `Disposable` which can be used to stop the invocation of the closure.
 	public func addObserver<T: MessageProtocol>(for messageType: T.Type, on queue: DispatchQueue, handler: @escaping (T, Peer) -> Void) -> Disposable {
 		return addDataObserver(on: queue) { data, peer in
 			do {
