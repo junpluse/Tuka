9478ea1c25d1b624db66e9ac99a8e93c66a40f09
Jun Tanaka <junpluse@gmail.com>
2016-12-09 00:15:22 +0900

    Move ResourceEvent definition to Session+Resource.swift

--- Sources/MultipeerConnectivity/Session+Resource.swift
+++ Sources/MultipeerConnectivity/Session+Resource.swift
@@ -9,6 +9,12 @@
 import Foundation
 import MultipeerConnectivity
 
+public enum ResourceEvent<Request: ResourceRequestProtocol> {
+	case transferStarted(Request, Session.Peer, Progress?)
+	case transferFinished(Request, Session.Peer)
+	case transferFailed(Request, Session.Peer, Error)
+}
+
 public final class ResourceRequestReceipt<Request: ResourceRequestProtocol>: RequestReceiptProtocol, Disposable {
 	public typealias Peer = Session.Peer
 
