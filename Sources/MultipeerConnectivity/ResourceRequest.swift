e4a998602bdda38b8ab697d48deabcd831c36193
Jun Tanaka <junpluse@gmail.com>
2016-12-14 20:38:26 +0900

    📝 Add documentation for ResourceRequest

--- Sources/MultipeerConnectivity/ResourceRequest.swift
+++ Sources/MultipeerConnectivity/ResourceRequest.swift
@@ -8,28 +8,45 @@
 
 import Foundation
 
+/// Represents a request message which be sent before transferring a resource
+/// to another peer in a session.
 public protocol ResourceRequestProtocol: RequestProtocol, SessionMessageProtocol {
 	associatedtype Response: ResourceResponseProtocol
 
+	/// A name for the resource.
 	var resourceName: String { get }
+
+	/// A mime type of the resource.
 	var mimeType: String? { get }
 }
 
-public final class ResourceRequest: ResourceRequestProtocol, KeyedCoding {
+/// A basic class which implements 'ResourceRequestProtocol'.
+public final class ResourceRequest: ResourceRequestProtocol {
 	public typealias Response = ResourceResponse
 
+	/// A string which identifies the request between peers.
 	public let requestID: String
+
+	/// A name for the resource.
 	public let resourceName: String
+
+	/// A mime type of the resource.
 	public let mimeType: String?
 
+	/// Initializes a resource request with values for requestID/resourceName/mimeType
+	///
+	/// - Parameters:
+	///   - requestID: A string which identifies the request between peers.
+	///   - resourceName: A name for the resource.
+	///   - mimeType: A mime type of the resource.
 	public init(requestID: String = UUID().uuidString, resourceName: String, mimeType: String? = nil) {
 		self.requestID = requestID
 		self.resourceName = resourceName
 		self.mimeType = mimeType
 	}
+}
 
-	// MARK: KeyedCoding
-
+extension ResourceRequest: KeyedCoding {
 	public enum CodingKey: String, CodingKeyPresentable {
 		case requestID
 		case resourceName
